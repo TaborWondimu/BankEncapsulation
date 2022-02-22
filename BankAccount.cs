088b3e65a1b1f25ea0851a4b2bbbfedabcf332fb
Tabor Wondimu <csmajortabor@gmail.com>
2022-02-21 22:19:33 -0800

    Finished Exercise

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+    {
+        public BankAccount()
+        {
+            // Default Constructor
+        }
+        private double balance = 0;
+    public void Deposit(double myDeposit)
+        {
+            balance += myDeposit;
+        }
+    public double GetBalance()
+        {
+          return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+           BankAccount myChaseAccount = new BankAccount();
+
+            Console.WriteLine("What is your deposit today?");
+            var myDeposit = double.Parse(Console.ReadLine());
+            myChaseAccount.Deposit(myDeposit);
+          
+            // this gets the balance
+            myChaseAccount.GetBalance();
+            
+            
+            Console.WriteLine($"You Deposited ${myDeposit},Your Current Balance is ${myChaseAccount.GetBalance()}"); 
+            
+     
+
+        }
+    }
+}
+
+
+
+
